6ab5332fdb6cff824acfa903c236d6bc5a9a1715
Michael Hennerich <michael.hennerich@analog.com>
2014-05-07 09:40:50 +0200

    adi_update_tools.sh: update
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- adi_update_tools.sh
+++ adi_update_tools.sh
@@ -9,13 +9,13 @@ BUILDS="fmcomms1-eeprom-cal \
 	iio-fm-radio \
 	linux_image_ADI-scripts"
 
-ARG2=""
-
 do_build ()
 {
   local prj=$1
   local arg=$2
-  make -j2 && make install $arg && echo "\n Building $prj finished Successfully\n" ||
+  local target=$3
+  make clean;
+  make -j3 $target && make install $arg && echo "\n Building $prj finished Successfully\n" ||
 	echo "Building $prj Failed\n"
 }
 
@@ -30,12 +30,14 @@ apt-get -y install libxml2 libxml2-dev bison flex
 for i in $BUILDS
 do
   cd /usr/local/src
+  ARG2=""
+  ARG_TARGET=""
 
   if [ -d $i ]
   then
     cd ./$i
     echo "\n *** Updating $i ***"
-    git pull || continue
+    git pull;
     cd ..
   else
     echo "\n *** Cloning $i ***"
@@ -53,12 +55,12 @@ do
   then
     git checkout origin/multi_plot_osc
     do_build "$i-multi_plot_osc"
-    make clean
     git checkout master
   elif [ $i = "libiio" ]
   then
     ARG2="PREFIX=/usr"
+    ARG_TARGET="libiio iiod"
   fi
 
-  do_build $i $ARG2
+  do_build $i $ARG2 $ARG_TARGET
 done
