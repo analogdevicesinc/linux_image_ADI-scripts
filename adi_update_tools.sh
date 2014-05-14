4bc6143b2934affe8ce57a57d1fff36e93c58d30
Robin Getz <robin.getz@analog.com>
2014-05-14 20:54:34 +0000

    update - compare downloaded update with installed one
    Signed-off-by: Robin Getz <Robin.Getz@analog.com>

--- adi_update_tools.sh
+++ adi_update_tools.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-BUILDS="fmcomms1-eeprom-cal \
+if [ "$(id -u)" != "0" ] ; then
+	echo "This script must be run as root"
+	exit 1
+fi
+
+#find md5of this file
+md5_self=`md5sum $0`
+
+# Keeps the scripts as the first thing, so we can check for updated 
+# scripts ...
+BUILDS="linux_image_ADI-scripts \
+	fmcomms1-eeprom-cal \
 	libiio \
 	iio-cmdsrv \
 	iio-oscilloscope \
 	fru_tools \
 	iio-cgi-netscope \
 	iio-fm-radio \
-	jesd-eye-scan-gtk \
-	linux_image_ADI-scripts"
+	jesd-eye-scan-gtk"
 
 do_build ()
 {
@@ -25,8 +35,6 @@ if [ -n "$1" ]
 then
 BUILDS=$1
 fi
-
-apt-get -y install libxml2 libxml2-dev bison flex
 
 for i in $BUILDS
 do
@@ -49,7 +57,21 @@ do
   cd ./$i
 
 # Handle some specialties here
-  if [ $i = "iio-cmdsrv" ]
+  if [ $i = "linux_image_ADI-scripts" ]
+  then
+    new=`md5sum ./adi_update_tools.sh`
+    if [ "$new" = "$md5_self" ] 
+    then
+      echo ./adi_update_tools.sh script is the same, continuing
+      # Now we are sure we are using the latest, make sure the pre-reqs are installed
+      apt-get -y install libgtkdatabox-0.9.1-1-dev libmatio-dev libxml2 libxml2-dev bison flex
+    else
+      # run the new one instead, and then just quit
+      echo ./adi_update_tools.sh has been updated, switching to new one
+      ./adi_update_tools.sh
+      exit
+    fi
+  elif [ $i = "iio-cmdsrv" ]
   then
     cd ./server
   elif [ $i = "iio-oscilloscope" ]
