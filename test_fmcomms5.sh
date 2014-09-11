f868db4b74c9b86024d349c5694ba9a44d319dcd
Tim Harder <timothy.harder@analog.com>
2014-09-11 15:21:22 -0400

    add script for auto-testing fmcomms5
    Signed-off-by: Tim Harder <timothy.harder@analog.com>

---
+++ test_fmcomms5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/usr/local/bin/osc -p /usr/local/lib/osc/profiles/FMComms5_test.ini
+rc=$?
+
+# save marker logs
+if [[ ${rc} -eq 0 ]] ; then
+	eeprom=$(find /sys -name eeprom -size 256c)
+	serial=$(fru-dump "${eeprom}" -b | awk '/^Serial Number/ {print $NF}')
+	[[ -d /root/osc-logs ]] || mkdir -p /root/osc-logs
+	mv log.txt /root/osc-logs/"${serial}"-log.txt
+fi
+
+/sbin/shutdown -h now
