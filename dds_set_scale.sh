0b5d9527da77b2a6cc68cfc968ad1f42f39af69e
Tim Harder <timothy.harder@analog.com>
2014-11-19 04:50:52 -0500

    dds_set_scale: simplify looping over device attrs
    Signed-off-by: Tim Harder <timothy.harder@analog.com>

--- dds_set_scale.sh
+++ dds_set_scale.sh
@@ -17,29 +17,25 @@ do
   dev_name=$(cat $i)
   if [ "$dev_name" = "cf-ad9122-core-lpc" ] || [ "$dev_name" = "axi-ad9144-hpc" ] ; then
     dac_path=$(echo $i | sed 's:/name$::')
-    attrs="\
-      out_altvoltage0_1A_scale \
+    attrs="out_altvoltage0_1A_scale \
       out_altvoltage1_1B_scale \
       out_altvoltage2_2A_scale \
-      out_altvoltage3_2B_scale \
-    "
+      out_altvoltage3_2B_scale"
   elif [ "$dev_name" = "cf-ad9361-dds-core-lpc" ] ; then
     dac_path=$(echo $i | sed 's:/name$::')
-    attrs="\
-      out_altvoltage0_TX1_I_F1_scale \
+    attrs="out_altvoltage0_TX1_I_F1_scale \
       out_altvoltage1_TX1_I_F2_scale \
       out_altvoltage2_TX1_Q_F1_scale \
       out_altvoltage3_TX1_Q_F2_scale \
       out_altvoltage4_TX2_I_F1_scale \
       out_altvoltage5_TX2_I_F2_scale \
       out_altvoltage6_TX2_Q_F1_scale \
-      out_altvoltage7_TX2_Q_F2_scale \
-    "
+      out_altvoltage7_TX2_Q_F2_scale"
   fi
 done
 
 mag=$(echo "scale=6; 1 / ( 2 ^ $1 )" | bc)
-echo $attrs | tr " " "\n" | while read attr
+for attr in $attrs
 do
   echo $mag > $dac_path/$attr
 done
