cca41d4aa803623d8b15118ca10c76ecb85414f4
Michael Hennerich <michael.hennerich@analog.com>
2021-12-17 17:24:56 +0100

    usb_otg.sh: Support all ZynqMP and Zynq-7000 platforms
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- usb_otg.sh
+++ usb_otg.sh
@@ -2,26 +2,37 @@
 
 # Copyright (c) 2021 Analog Devices, Inc. All Rights Reserved.
 # This software is proprietary to Analog Devices, Inc. and its licensors.
-# By using this software you agree to the terms of the associated 
+# By using this software you agree to the terms of the associated
 # Analog Devices Software License Agreement.
 
 # Manages USB OtG configuration in devicetree for supported platforms
 
 
+is_compatible() {
+	echo $COMP | grep -q $1
+	return $?
+}
+
 check_platform() {
 	# Most ARM systems will fill /sys/firmware.
-	MODEL="/sys/firmware/devicetree/base/model"
-	if [ -f "${MODEL}" ] ; then
-		BASE=$(tr -d '\0' < $MODEL)
-		echo "Board=$BASE"
+	COMPATIBLE="/proc/device-tree/compatible"
+	if [ -f "${COMPATIBLE}" ] ; then
+		COMP=$(strings /proc/device-tree/compatible)
+		echo "Compatible=$COMP"
 	else
 		echo "[ERROR] No Model information in devicetree"
 		exit 1
 	fi
 
-	if [ "$BASE" = "Xilinx Zynq ZED" ] ; then
+	if $(is_compatible "xlnx,zynq-7000"); then
 		echo "USB_OTG_Supported=Yes"
-	else 
+		USB="/amba/usb@e0002000"
+		DTB="/boot/devicetree.dtb"
+	elif $(is_compatible "xlnx,zynqmp"); then
+		echo "USB_OTG_Supported=Yes"
+		USB="/axi/usb0@ff9d0000/dwc3@fe200000"
+		DTB="/boot/system.dtb"
+	else
 		# All other platform are not supported
 		echo "USB_OTG_Supported=No"
 		exit 1
@@ -29,19 +40,19 @@ check_platform() {
 }
 
 show_status() {
-	echo "USB_OTG_Status=" $(fdtget -t s /boot/devicetree.dtb /amba/usb@e0002000 dr_mode)
+	echo "USB_OTG_Status=" $(fdtget -t s $DTB $USB dr_mode)
 }
 
 case "$1" in
 enable)
-	check_platform	
-	fdtput -t s /boot/devicetree.dtb /amba/usb@e0002000 dr_mode otg
+	check_platform
+	fdtput -t s $DTB $USB dr_mode otg
 	show_status
 	;;
 
 disable)
 	check_platform
-	fdtput -t s /boot/devicetree.dtb /amba/usb@e0002000 dr_mode host
+	fdtput -t s $DTB $USB dr_mode host
 	show_status
 	;;
 
