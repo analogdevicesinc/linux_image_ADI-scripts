881ecbab2af4b7cf555d29bc26a18b49e90421f7
Cosmin Tanislav <demonsingur@gmail.com>
2022-07-26 12:55:10 +0300

    Add Stingray power up/down script
    Signed-off-by: Cosmin Tanislav <demonsingur@gmail.com>

--- Makefile
+++ Makefile
@@ -3,4 +3,12 @@ DESTDIR=/usr/local
 install:
 	install -d $(DESTDIR)/bin
 	install ./*.sh $(DESTDIR)/bin/
+
+	install -D -m 0644 ./power-service/adi-power.service /etc/systemd/system/
+	install -D -m 0644 ./power-service/adi_power.py /usr/share/systemd/
+	install -D -m 0644 ./power-service/stingray_power.py /usr/share/systemd/
+
+	systemctl daemon-reload
+	systemctl enable adi-power.service
+
 	/bin/sh usb-gadget-service/install_gt.sh
---
+++ power-service/adi_power.py
@@ -0,0 +1,28 @@
+import sys
+import subprocess
+
+MODEL_PATH = '/sys/firmware/devicetree/base/model'
+
+def read_file(path):
+	with open(path, 'r') as file:
+		return file.read()
+
+def read_model_file():
+	# Device tree strings are null-terminated
+	return read_file(MODEL_PATH).strip('\0')
+
+def run_py_script(path, args):
+	subprocess.run(['python3', path, *args])
+
+if __name__ == '__main__':
+	model = read_model_file()
+
+	print(f'Found model name: {model}')
+
+	SYSTEMD_PATH = '/usr/share/systemd'
+
+	STINGRAY_POWER_PATH = f'{SYSTEMD_PATH}/stingray_power.py'
+	STINGRAY_V1_0_MODEL = 'Stingray ZynqMP ZCU102 Rev1.0'
+
+	if model == STINGRAY_V1_0_MODEL:
+		run_py_script(STINGRAY_POWER_PATH, [sys.argv[1]])
