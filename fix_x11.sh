5c8ecc78e72e4ef66c02864a574051e92bec8503
mlradu <larisa.radu@analog.com>
2024-09-16 16:37:49 +0300

    fix_x11: fix 'if' statement
    In 'if' statement, 'grep' always returned exit code to enter the first branch, no matter the contents of the checked file.
    Fixed that by removing the evaluation of the conditional expressions ( '[]' ).
    
    Signed-off-by: mlradu <larisa.radu@analog.com>

--- fix_x11.sh
+++ fix_x11.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! $(grep -qi -e "ZCU102" -e "ADRV9009-ZU" -e "Jupiter SDR" /sys/firmware/devicetree/base/model) ]; then
+if grep -qi -e "ZCU102" -e "ADRV9009-ZU" -e "Jupiter SDR" /sys/firmware/devicetree/base/model; then
 	# create X11 xorg.conf
 	printf "Section \"Device\"\n  Identifier \"myfb\"\n  Driver \"fbdev\"\n  Option \"fbdev\" \"/dev/fb0\"\nEndSection\n" \
 		> /etc/X11/xorg.conf \
