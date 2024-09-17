6f7375703313a839f837b1b6a4984f3a1daa5f3b
Stefan Raus <Stefan.Raus@analog.com>
2024-09-17 11:12:44 +0100

    fix_x11: remove xorg.conf only for non-DP platforms
    Signed-off-by: Stefan Raus <Stefan.Raus@analog.com>

--- fix_x11.sh
+++ fix_x11.sh
@@ -3,9 +3,10 @@
 if grep -qi -e "ZCU102" -e "ADRV9009-ZU" -e "Jupiter SDR" /sys/firmware/devicetree/base/model; then
 	# create X11 xorg.conf
 	printf "Section \"Device\"\n  Identifier \"myfb\"\n  Driver \"fbdev\"\n  Option \"fbdev\" \"/dev/fb0\"\nEndSection\n" \
-		> /etc/X11/xorg.conf \
-		|| rm  /etc/X11/xorg.conf
+		> /etc/X11/xorg.conf
 
 	# delete xorg.conf created by enable_dummy_display if exists
 	rm -f /usr/share/X11/xorg.conf.d/xorg.conf
+else
+	rm -f /etc/X11/xorg.conf
 fi
