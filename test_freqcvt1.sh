be5dc26a4ee2f6ebf6b4ffb7a9d629202b0e1b94
mlradu <larisa.radu@analog.com>
2026-04-17 12:53:12 +0300

    Temporarily remove tests
    In the next release will fix their bugs and add them back.
    
    Signed-off-by: mlradu <larisa.radu@analog.com>

--- test_freqcvt1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/osc -p /usr/local/lib/osc/profiles/AD-FREQCVT1_test.ini
-ret=$?
-
-if [[ ${ret} -eq 0 ]] ; then
-	/sbin/shutdown -h now
-fi
--- test_trxboost1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/osc -p /usr/local/lib/osc/profiles/AD-TRXBOOST1_test.ini
-ret=$?
-
-if [[ ${ret} -eq 0 ]] ; then
-	/sbin/shutdown -h now
-fi
